f379575f82322b7d93aa3bb3938fd1ce5d986607
Shivam Singh <36638763+shivamsingh17@users.noreply.github.com>
2025-03-03 15:26:16 +0530

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Graph
+{
+    void dfs(ArrayList<ArrayList<Integer>> g)
+    {
+        
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+// 		Map<Integer, List<Integer> > adjacencyList;
+ArrayList<Integer> a = new ArrayList<Integer>();
+a.add(1);
+a.add(2);
+
+		adj.add(0, a);
+		System.out.println(adj);
+
+	}
+}
