d8e106942f4f865dfd6c306b7219a01863d8b37f
Shivam Singh <36638763+shivamsingh17@users.noreply.github.com>
2025-03-03 17:51:08 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,17 +3,36 @@ import java.lang.*;
 import java.io.*;
 
 class BinarySearch
-{
-    int bin(int[] a)
+{   int firstandlast(int []a){
+    int res = -1;
+            int t = 3;
+
+          int s = 0;
+        int end = a.length - 1;
+        int mid = 0;
+        while(s<=end){ // r
+            mid = (s+end)/2;
+            if(t == a[mid]){
+                res = mid; 
+                end = mid-1;; // 1st oc 
+                // last oc start = mid+1
+            }
+            else if(t < a[mid]) end = mid-1;
+            else s = mid+1;
+        }
+        
+        return res;
+}
+    int bin(int[] a) //naive
     {
         int t = 1;
         int s = 0;
         int end = a.length - 1;
         int mid = 0;
-        while(s<=end){ //
+        while(s<=end){ // r
             mid = (s+end)/2;
             if (t == a[mid])return mid;
-            if(t>a[mid])s = mid+1;//
+            if(t>a[mid])s = mid+1;//r
             else
             end = mid-1;
             
@@ -21,12 +40,16 @@ class BinarySearch
         return -1;
         
     }
+    
+    
 	public static void main (String[] args) throws java.lang.Exception
 	{BinarySearch ob = new BinarySearch();
 		// your code goes here
-	int a[] = {2,3,4,6,8};
+	int a[] = {1, 2,3,3, 4,6,8};
 	System.out.println(ob.bin(a));
+		System.out.println(ob.firstandlast(a));
 	
+
 
 	}
 }
