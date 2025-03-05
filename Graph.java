42bf6ab76b53e8261033f64dd33ef615572d2a3b
Shivam Singh <36638763+shivamsingh17@users.noreply.github.com>
2025-03-05 23:28:40 +0530

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,24 +1,56 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Graph
-{
-    void dfs(ArrayList<ArrayList<Integer>> g)
-    {
-        
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    private boolean dfs(int node, List<List<Integer>> adj, boolean[] visited, int parent) {
+        visited[node] = true;
+
+        for (int nbr : adj.get(node)) {
+            if (!visited[nbr]) {
+                boolean ans = dfs(nbr, adj, visited, node);
+                if (ans) return true;  // Found a cycle
+            } 
+            else if (nbr != parent) {
+                return true; // Back edge found, cycle detected
+            }
+        }
+        return false;
     }
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-// 		Map<Integer, List<Integer> > adjacencyList;
-ArrayList<Integer> a = new ArrayList<Integer>();
-a.add(1);
-a.add(2);
 
-		adj.add(0, a);
-		System.out.println(adj);
+    public boolean isCycle(int V, List<List<Integer>> adj) {
+        boolean[] visited = new boolean[V];
 
-	}
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, adj, visited, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Main function for testing
+    public static void main(String[] args) {
+        int V = 5;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Adding edges to represent an undirected graph
+        adj.get(0).add(1);
+        adj.get(1).add(0);
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+        adj.get(3).add(4);
+        adj.get(4).add(3);
+        adj.get(4).add(1); // Adding a back edge to form a cycle
+        adj.get(1).add(4);
+
+        Solution sol = new Solution();
+        System.out.println("Cycle Detected: " + sol.isCycle(V, adj));
+    }
 }
